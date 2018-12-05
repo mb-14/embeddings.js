63d893e23cb0a6d94eabd91a46e9ee9bc9b9a20b
Miroojin <mbakshi1094@gmail.com>
2018-12-05 16:17:40 +0530

    Update readme, clean up python script

--- README.md
+++ README.md
@@ -2,4 +2,54 @@
 
 Word embeddings for the the web
 
-The project 
+Word embeddings often require a large number of parameters which results in a large memory and storage footprint.
+This makes deploying pre-trained word embeddings like [fastText](https://fasttext.cc/) and [GloVe](https://nlp.stanford.edu/projects/glove/) in mobile and browser environments very difficult. In this project,
+we will compress pre-trained word vectors using simple post-processing techniques like **PCA dimensionality reduction** and **production quantization**.
+The resulting embeddings are significantly smaller compared to the original embeddings with no considerable drop in accuracy.
+
+## Instructions
+
+### Downloading pre-trained word vectors
+Run `fetch_vectors.sh` to download pre-trained vectors from fastText trained on the common crawl dataset. The file contains 50k word vectors, each of 300 dimensions.
+```console
+foo@bar:~$ ./fetch_vectors.sh
+Downloading fastText word vectors for 50k most common words...
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100 35.5M  100 35.5M    0     0  1272k      0  0:00:28  0:00:28 --:--:-- 1278k
+Archive:  data/crawl-300d-50K.vec.zip
+  inflating: data/crawl-300d-50K.vec
+
+```
+### Loading vectors into gensim
+Run `main.py` to load the words vectors into gensim and calculate the accuracy using the word analogies test.
+```console
+foo@bar:~$ python main.py -i data/crawl-300d-50K.vec
+Loading vectors in gensim...
+Calculating accuracy...
+Accuracy: 87.078803%
+``` 
+
+### Compressing embeddings
+Add `-c` parameter to compress the embeddings and re-calculate the accuracy. The compressed embeddings, codebook of centroids and vocabulary as saved in the model directory as JSON files.
+
+```
+foo@bar:~$ python main.py -c -i data/crawl-300d-50K.vec
+Loading vectors in gensim...
+Reduce dimensions using PCA...
+Size reduction: 50.000000%
+Compress embeddings using product quantization...
+Size reduction: 93.000000%
+Calculating accuracy...
+Accuracy: 81.957310%
+Saving model/vocab.json
+Saving model/codewords.json
+Saving model/embeddings.json
+```
+
+### Building JS library
+Run `gulp build` to generate the js library and the final model JSON file in the `dist` directory
+
+
+
+## Example
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import logging
+import argparse
 import json
 import codecs
+import logging
 import os
+import warnings
 
 from sklearn.decomposition import PCA
 from gensim.models import KeyedVectors
 import pqkmeans
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def reduce_dimensions_pca(vectors, dimensions=150):
@@ -14,12 +18,17 @@ def reduce_dimensions_pca(vectors, dimensions=150):
 
 
 def product_quantize(vectors, subdims=30, centres=1000):
-    encoder = pqkmeans.encoder.PQEncoder(num_subdim=subdims, Ks=centres)
+    encoder = pqkmeans.encoder.PQEncoder(iteration=40, num_subdim=subdims, Ks=centres)
     encoder.fit(vectors)
     vectors_pq = encoder.transform(vectors)
     reconstructed_vectors = encoder.inverse_transform(vectors_pq)
     return reconstructed_vectors, vectors_pq, encoder.codewords
 
+
+def compute_accuracy(model):
+    print("Calculating accuracy...")
+    accuracy, _ = model.evaluate_word_analogies('questions-words.txt', restrict_vocab=50000)
+    print("Accuracy: {:f}%".format(accuracy*100))
 
 def save_matrix(name, matrix):
     matrix_shape = list(matrix.shape)
@@ -28,41 +37,59 @@ def save_matrix(name, matrix):
         "shape": matrix_shape,
         "vectors": matrix_list
     }
-    with open(os.path.join('model', "{:s}.json".format(name)), 'w') as f:
+    file = os.path.join("model", "{:s}.json".format(name))
+    with open(file, 'w') as f:
+        print("Saving {:s}".format(file))
         f.write(json.dumps(data))
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    model = KeyedVectors.load_word2vec_format('data/wiki-news-300d-50k.vec')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", dest="filename", required=True,
+                        help="input file containing word embeddings")
+    parser.add_argument("-c", dest="compress", help="compress word embeddings", action="store_true")
+    parser.add_argument("-v", dest="verbose", help="verbosity", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    print("Loading vectors in gensim...")
+    model = KeyedVectors.load_word2vec_format(args.filename)
+
+    if not args.compress:
+        compute_accuracy(model)
+        exit(0)
+
     embeddings = model.vectors
     size = embeddings.nbytes
 
     print("Reduce dimensions using PCA...")
     embeddings = reduce_dimensions_pca(embeddings)
+    new_size = embeddings.nbytes
+    print("Size reduction: {:f}%".format((size - new_size) * 100 / size))
+
     print("Compress embeddings using product quantization...")
     embeddings, embeddings_pq, codewords = product_quantize(embeddings)
-
-    print("Calculating accuracy...")
-    words = [model.index2word[idx] for idx in range(len(embeddings))]
-    model = KeyedVectors(vector_size=embeddings.shape[1])
-    model.add(words, embeddings, replace=True)
-    model.evaluate_word_analogies('questions-words.txt', restrict_vocab=50000)
 
     new_size = embeddings_pq.nbytes + codewords.nbytes
     print("Size reduction: {:f}%".format((size-new_size)*100/size))
 
+    words = [model.index2word[idx] for idx in range(len(embeddings))]
+    model = KeyedVectors(vector_size=embeddings.shape[1])
+    model.add(words, embeddings, replace=True)
+    compute_accuracy(model)
+
     if not os.path.exists("model"):
         os.makedirs("model")
 
-    print("Saving vocabulary...")
-    with codecs.open('model/vocab.json', 'w', 'UTF-8') as f:
+    vocab_file_path = os.path.join("model", "vocab.json")
+    with codecs.open(vocab_file_path, 'w', 'UTF-8') as f:
+        print("Saving {:s}".format(vocab_file_path))
         f.write(json.dumps(words))
 
-    print("Saving codewords...")
     save_matrix('codewords', codewords)
-
-    print("Saving embeddings...")
     save_matrix('embeddings', embeddings_pq)
 
 
