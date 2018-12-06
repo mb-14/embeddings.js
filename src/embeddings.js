bcf0546911adaa9599c384487116babd93d74aba
Miroojin <mbakshi1094@gmail.com>
2018-12-06 15:43:29 +0530

    Revert async

--- src/embeddings.js
+++ src/embeddings.js
@@ -41,7 +41,7 @@ class WordEmbeddings {
 	}
 
 	// getNearestNeighbors returns the closest k word vectors from a given word vector 
-	async getNearestNeighbors(word, k=5) {
+	getNearestNeighbors(word, k=5) {
 		var neighbors = [];
 		var embedding = this.embeddingsMap[word];
 		var vector = this._getVector(word);
